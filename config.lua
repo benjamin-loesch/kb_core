012f332bb14df13a106d3b22371c9ee8d2dce4eb
Benjamin Loesch <benjamin.loesch@gmail.com>
2021-07-28 16:24:01 +0200

    reduced loading of scripts

--- config.lua
+++ config.lua
@@ -3,7 +3,7 @@ Config.Locale = 'en'
 
 Config.EnableHud                = false     -- enable the default hud? Display current job and accounts (black, bank & cash)
 Config.MaxWeight                = 24        -- the max inventory weight without backpack
-Config.EnableDebug              = false     -- Use Debug options?
+Config.EnableDebug              = true     -- Use Debug options?
 Config.EnableDefaultInventory   = true      -- Display the default Inventory ( F2 )
 Config.EnableWantedLevel        = false     -- Use Normal GTA wanted Level?
 Config.EnablePVP                = true      -- Allow Player to player combat
--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,18 +3,19 @@ fx_version 'cerulean'
 games { 'gta5' }
 
 author 'Benjamin Loesch'
-description 'Property inclusion for Los Santos'
+description 'KangarooB package: Core for Los Santos'
 version '0.0.1'
 
 shared_scripts {
-  'locale.lua',
-  'locales/en.lua',
-
+--  'locale.lua',
+--  'locales/en.lua',
+--
   'config.lua',
 }
 server_scripts {
   '@async/async.lua',
   '@mysql-async/lib/MySQL.lua',
+  'events.lua'
 }
 
 client_scripts {
